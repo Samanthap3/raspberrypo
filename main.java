5292c68320fb1c9fd6baf87e59dd7b7bb2e48b39
Athena Chen <afchen@stu.naperville203.org>
2023-09-01 13:10:52 -0500

    weird stuffs

---
+++ main.java
@@ -0,0 +1,8 @@
+// hi! main
+public class emmadram
+{
+public static void main(String[] args)
+{
+    System.out.println("hello");
+}
+}
---
+++ testjava/src/athena.java
@@ -0,0 +1,6 @@
+public class athena {
+    public static void main(String[] args)
+    {
+        System.out.print("hi samantha");
+    }
+}
