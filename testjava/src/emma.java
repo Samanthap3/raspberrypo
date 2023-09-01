561346e76393126af7b5218c9459740200eb6d98
EmmaDram <esdram@stu.naperville203.org>
2023-09-01 13:17:05 -0500

    hdfdfgfgfdggf

---
+++ testjava/src/emma.java
@@ -0,0 +1,15 @@
+public class emma
+{
+    public static void main(String[] args)
+    {
+        System.out.print("emma's class");
+
+        message();
+    }
+
+    public static String message()
+    {
+        
+        return "PUSH BUTTON";
+    }
+}
