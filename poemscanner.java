65d2ae3c623142fa9e642291d30e5d7a06967c9a
Samanthap3 <span@stu.naperville203.org>
2023-09-02 14:19:40 -0500

    scanner for poem files

---
+++ poemscanner.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+public class poemscanner{
+
+    public static void main(String args[]) throws IOException
+    {
+        File pfile1 = new File("poem1.txt");
+        File pfile2 = new File("poem2.txt");
+        File pfile3 = new File("poem3.txt");
+
+        Scanner ss = new Scanner(pfile1);
+        Scanner ws = new Scanner(pfile2);
+        Scanner ccm = new Scanner(pfile3);
+    }
+}
