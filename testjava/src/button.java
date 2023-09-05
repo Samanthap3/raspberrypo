ea91a7d6f50d4a612d58e03ed8e84310f557f0d9
EmmaDram <esdram@stu.naperville203.org>
2023-09-05 13:18:13 -0500

    askdkasdj

--- testjava/src/button.java
+++ testjava/src/button.java
@@ -1,3 +1,24 @@
-public class button {
-    
+import java.util.*;
+
+public class button
+{
+    static Random random = new Random();
+    public static void main(String[] args)
+    {
+        System.out.println(message());
+    }
+
+    public static String message()
+    {
+        return "PUSH BUTTON";
+    }
+
+    public static int generatePoem(/*Poems[] array*/)
+    {
+        /*placeholder for correct code*/int sizeOfArray = 0;
+        int key = random.nextInt(/*array.length*/sizeOfArray);
+
+        return key;
+        //return array[key];
+    }
 }
