24dbec6d6885b412ec3876617a92b3abcd8cb16a
Athena Chen <afchen@stu.naperville203.org>
2023-09-05 13:29:27 -0500

    PUSH AND FETCH

--- testjava/src/Main.java
+++ testjava/src/Main.java
@@ -1,6 +1,15 @@
+import java.util.*;
+import java.io.*;
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args)
     {
-        System.out.print("this is me");
+        Poem samplepoem = new Poem(null);
+        System.out.println()
+        Poem(generatePoem());
+        System.out.print(samplepoem.title);
+        System.out.print(samplepoem.author);
+
     }
 }
